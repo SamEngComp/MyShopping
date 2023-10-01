9c69e130d22d943da36ecee6c4440045045b6e2f
Samuel S. Furtado <salessamuel268@gmail.com>
2023-10-01 17:59:18 -0300

    feat: add Wallet class

---
+++ src/Domain/Wallet.java
@@ -0,0 +1,18 @@
+package Domain;
+
+public class Wallet {
+
+    // PROPERTIES
+
+    private double totalBalance;
+
+    // GET AND SET
+
+    public double getTotalBalance() {
+        return totalBalance;
+    }
+
+    public void setTotalBalance(double totalBalance) {
+        this.totalBalance = totalBalance;
+    }
+}
